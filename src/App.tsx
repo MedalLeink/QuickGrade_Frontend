cf33db8127dfe1ffd6bab25194ab9eb4dcceb60b
Akem-Ben <akemini.ndaobong@gmail.com>
2024-01-28 18:45:14 +0100

    setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+
+
+//add the path and components to the element, you may remove the fragment
+
+const App = () => {
+  return (
+    <>
+    <Router>
+          <Routes>
+          </Routes>
+    </Router>
+    </>
+  );
+};
+
+export default App;
