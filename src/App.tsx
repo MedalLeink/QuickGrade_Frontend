d5fe3c15c148feaea96abdb82dadaae48f93ca73
Decagon <decagon@Sandra-Macbook.local>
2024-02-08 01:42:35 +0100

    student

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import LecturerDashboard from "./pages/LecturerDashboard";
 import StudentDashboard from "./pages/StudentDashboard";
 
 //add the path and components to the element, you may remove the fragment
@@ -9,7 +8,6 @@ const App = () => {
     <>
     <Router>
           <Routes>
-            <Route path="/LecturerDashboard" element={<LecturerDashboard />}/>
             <Route path="/StudentDashboard" element={<StudentDashboard />}/>
 
           </Routes>
