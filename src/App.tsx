86d15cc960c62cbba45c2fe732de8fbd54de00b5
MedalLeink <nkiru.anagha@decagon.dev>
2024-02-09 06:23:08 -0500

    Sign in Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import LaunchScreen from "./pages/LaunchScreen";
 import SignInPage from "./pages/SignInPage";
+import ForgetPassword from "./pages/forgetPassword";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LaunchScreen />} />
         <Route path="/SignInPage" element={<SignInPage />} />
+        <Route path="/forgetPassword" element={<ForgetPassword />} />
         <Route path="/sidebar" element={<Sidebar />} />
         <Route path="/navbar" element={<Navbar name={"22/23/07/019"} />} />
       </Routes>
