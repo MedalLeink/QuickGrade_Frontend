aa4a4960ef6204920fc39c7f12e7ba2dc0bcb689
mac <praise.etiuma@decagon.dev>
2024-02-16 05:47:18 +0100

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
 
 
 //add the path and components to the element, you may remove the fragment
@@ -12,6 +14,8 @@ const App = () => {
           <Routes>
           <Route path="/sidebar" element={<Sidebar />} />
           <Route path="/navbar" element={<Navbar name={"22/23/07/019"} />} />
+          <Route path="/modal" element={<Modal />} />
+          <Route path="/button" element={<Button text={"white"} bg={"blue"} title="Submit"/>} />
           </Routes>
     </Router>
     // </>
