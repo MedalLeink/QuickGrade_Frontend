8d32fd5a986de555eb03aeb3bda67481b03cc5f9
MedalLeink <nkiru.anagha@decagon.dev>
2024-02-25 15:01:35 -0500

    enrolled courses

--- src/components/SessionCalendar.tsx
+++ src/components/SessionCalendar.tsx
@@ -1,25 +1,11 @@
 import React, { useState } from "react";
 
 const SessionCalendar = () => {
-  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedYear] = useState("2022/2023");
 
-  // Function to handle year selection
-  const handleYearChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
-    setSelectedYear(e.target.value);
-    // You can perform additional actions based on the selected year if needed
-  };
-
-  // Get the current year
-  const currentYear = new Date().getFullYear();
-
-  // Generate options for the previous and current years
-  const options = Array.from({ length: currentYear - 1960 + 1 }, (_, index) => {
-    const year = currentYear - index;
-    return { value: `${year - 1}/${year}`, label: `${year - 1}/${year}` };
-  });
 
   return (
-    <div className="w-295 h-44">
+    <div className="w-295 h-10 flex items-center">
       <label
         htmlFor="session"
         className="w-101 h-34 font-inter text-24 font-bold leading-34"
@@ -27,20 +13,12 @@ const SessionCalendar = () => {
       >
         Session:
       </label>
-      <select
-        name="session"
-        id="session"
-        className="w-101 h-34 font-bold text-24 p-2 font-inter leading-34 tracking-0 text-left"
+      <div
+        className="w-101 h-34 font-bold text-24 p-2 font-inter leading-34 tracking-0 text-left bg-blue-50 bg-opacity-50 ml-4"
         style={{ color: "#667085" }}
-        onChange={handleYearChange}
-        value={selectedYear}
       >
-        {options.map((option) => (
-          <option key={option.value} value={option.value} className="">
-            {option.label}
-          </option>
-        ))}
-      </select>
+        {selectedYear}
+      </div>
     </div>
   );
 };
